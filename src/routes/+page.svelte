ecf07a4d795f7c50a07ea9da776ccc65a4b2cf16
joemmalatesta <joemmalatesta@gmail.com>
2023-03-26 03:04:02 -0400

    finished everything. Next is responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,20 @@
+<!-- This shit is all over the place and I hope to make improvements but it looks and works really good :D -->
+<!-- Really shouldn't be in one file, but really why not? -->
+<!-- TODO: Make this responsive. -->
+<!-- TODO: make this a PWA as well, that would be cool -->
+
+
+
 <script lang="ts">
 	import { each, onMount, space, onDestroy } from "svelte/internal";
 	import { fly, slide, fade, blur, crossfade } from "svelte/transition";
+
+	//Make python sleep because idk how setTimeout works
 	const sleep = (milliseconds: number) => {
 		return new Promise((resolve) => setTimeout(resolve, milliseconds));
 	};
+
+	//Initialize all Categories and Hover Elements
 	let allCategories: string[] = [
 		"A Boy's Name",
 		"U.S. Cities",
@@ -212,19 +223,26 @@
 	];
 	let pauseHovered = false;
 	let refreshHovered = false;
-    let refreshRequired: boolean = false;
 
-	// CHOSE OR REFRESH LETTER
+
+
+	// CHOOSE OR REFRESH LETTER
 	let letter: string = randomLetter();
 	function randomLetter() {
-		const characters = "abcdefghijklmnopqrstuvwxyz";
+		//Only scattergories letters
+		const characters = "abcdefghijklmnoprstw";
 		return characters.charAt(Math.floor(Math.random() * characters.length)).toUpperCase();
 	}
 
+
+
 	//   CHOOSE CATEGORIES
+	//  empty and choose 12 new categories each time refresh is run.
+	//  Remove selected categories from the big list so that they're not reused.
 	let possibleCategories: string[] = allCategories;
 	let categories: string[] = refreshCategories();
 	function refreshCategories() {
+		//Refresh the letter as well on list refresh
 		letter = randomLetter();
 		let selectedCategories = [];
 		for (let i = 0; i < 12; i++) {
@@ -236,65 +254,82 @@
 		return selectedCategories;
 	}
 
-	//SET AND COUNT DOWN TIMER... MAKE IT STOP ON DESTROY BUT IDK HOW.
-	let resetTo: number = 120;
+
+
+	//SET AND COUNT DOWN TIMER...
+	//Should probably make this pause on leaving the page but it should be fine
+	//Number always resets back to time entered by user (resetTo)
+	//StartTimer and pause timer do exactly that and are called by the later "$:" statement
+
+	let resetTo: number = 120; //Bound to value of change time
 	let time = resetTo;
-    let interval: any
-    let alertTime = false
-    function startTimer() {
-  paused = false;
-  interval = setInterval(function() {
-    if (time === 0) {
-      clearInterval(interval);
-      alertTime = true
-      animateRefresh(true)
-    } else {
-      time--;
-    }
-  }, 1000);
-}
+	let interval: any;
+	let alertTime = false;
+	function startTimer() {
+		paused = false;
+		interval = setInterval(function () {
+			//If out of time, run animate saying "Time's up." 
+            //AlertTime is used because text needs to be replaced before the second animateRefresh call for actual refreshing
+			if (time === 0) {
+				clearInterval(interval);
+				alertTime = true;
+				animateRefresh(true);
+			} else {
+				time--;
+			}
+		}, 1000);
+	}
 
-function stopTimer() {
-  clearInterval(interval);
-  paused = true;
-}
-    
-
-
-	let changeTime = false;
-	function confirmTimeClicked() {
-		time = resetTo;
-		changeTime = false;
+	function stopTimer() {
+		clearInterval(interval);
+		paused = true;
 	}
 
 
-	// Set paused text and Icon
+    //Used for changing the time. ChangeTimeSelected to handle clicks that aren't confirm.
+	let changeTimeSelected = false;
+	function confirmTimeClicked() {
+        if (resetTo > 1000 || resetTo <= 0){
+            resetTo = 120
+        }
+		time = resetTo;
+		changeTimeSelected = false;
+	}
+
+
+
+
+
+	// Set text and Icon for pause/play box depending on the paused state
 	let playPause: string, pausedIcon: string;
 	let paused = true;
 	$: if (!paused) {
-        startTimer();
+		startTimer();
 		playPause = "Pause";
 		pausedIcon = "/pause.webp";
-	} 
-    else {
-        stopTimer();
+	} else {
+		stopTimer();
 		playPause = "Play";
 		pausedIcon = "/play.png";
 	}
 
 
-	// Animate Refresh
-	let showReset = false;
-	let spinRefresh = false;
+
+
+
+	// Animate Refresh. Either for resetting or when the time runs out.
+	let showReset = false; //Covers the background completely
+	let spinRefresh = false; //Animate the spinny image and accompanying text
 	async function animateRefresh(timesUp: boolean = false) {
-        if (timesUp) {
-            showReset = true;
-		    await sleep(2000);
-		    showReset = false;
-            alertTime = false
-            return
-        }
-		spinRefresh = true;
+		if (timesUp) {
+			showReset = true; //Swipe in 
+			await sleep(2000); //Let layer stay longer when time is up
+			showReset = false; // Swipe out
+			alertTime = false;
+			return;
+		}
+        //Spin and wait a sec, then swipe in and out with a one second delay
+		spinRefresh = true; 
 		await sleep(200);
 		showReset = true;
 		await sleep(1000);
@@ -303,10 +338,15 @@ function stopTimer() {
 	}
 </script>
 
+
+<!-- Whole thing is just a big ole grid.-->
 <div class="flex h-[97vh] scale-95 font-serif text-[#222222]">
+
+
 	<!-- FIRST COLUMN -->
 	<div class="flex flex-col h-full w-[30%]">
-		<!-- Top Half -->
+
+		<!-- Top Half, just the letter. Reroll function is pretty simple -->
 		<div class="relative h-1/2 border-4 border-neutral-900 border-b-0 stripes flex justify-center items-center">
 			<div class="top-0 absolute flex justify-between w-full">
 				<p class="text-2xl left-0 mx-3 my-1">Letter</p>
@@ -317,35 +357,43 @@ function stopTimer() {
 					}}>Refresh</button
 				>
 			</div>
-			<h1 class="font-semibold font-sans" style="font-size: 18rem ;">{letter}</h1>
+			<h1 class="font-semibold font-sans text-[18rem]">{letter}</h1>
 		</div>
-		<!-- Bottom Half -->
+
+
+
+
+		<!-- Bottom Half, time and pause/play. A lot happening here so read more in depth -->
 		<div class="flex h-1/2">
-			<!-- Time -->
+
+			<!-- Time. Shows time and allows for changing time. When time is changed, it is saved to be used in future rounds. -->
 			<div class="w-1/2 border-4 border-neutral-900 border-r-0 relative flex justify-center items-center">
 				<div class="top-0 absolute flex justify-between w-full">
 					<p class="text-2xl left-0 mx-3 my-1">Time</p>
 					<button
 						class="text-2xl left-0 mx-3 my-1 text-neutral-900/70 hover:text-neutral-900/90 transition-all"
 						on:click={() => {
-                            paused = true
-							if (changeTime) {
-                                confirmTimeClicked()
-                            }
-                            else{ changeTime = true};
+							paused = true;
+							if (changeTimeSelected) {
+								confirmTimeClicked();
+							} else {
+								changeTimeSelected = true;
+							}
 						}}>Change</button
 					>
 				</div>
-
-				{#if changeTime}
+                
+                <!-- When change is selected, open an editor of sorts. Clicking confirm, change again, or pressing play are all valid escapes -->
+				{#if changeTimeSelected}
 					<form class="flex flex-col items-center justify-center">
 						<input class="border-2 border-neutral-800 border-b-0 text-3xl w-44 h-10" type="number" placeholder={`${time - 1}`} bind:value={resetTo} />
 						<button class="w-full bg-neutral-800 text-white hover:bg-neutral-900 h-8" on:click={confirmTimeClicked}>Confirm</button>
-                    </form>
+					</form>
 				{:else}
 					<h1 class="text-8xl">{time}</h1>
 				{/if}
 			</div>
+
 			<!-- PLAY / PAUSE -->
 			<div
 				class="w-1/2 border-4 border-neutral-900 relative flex justify-center items-center {pauseHovered ? 'dots' : ''}"
@@ -356,6 +404,7 @@ function stopTimer() {
 					pauseHovered = false;
 				}}
 			>
+            <!-- Key makes it so that each time paused changes, the animations are reset. Most of this stuff is handled in the script portion though-->
 				{#key paused}
 					<div class="absolute top-0 flex w-full">
 						<p class="text-2xl my-1 mx-3" transition:fly={{ delay: 100, duration: 400, y: 20 }}>{playPause}</p>
@@ -367,9 +416,15 @@ function stopTimer() {
 						in:fly={{ delay: 100, x: -20 }}
 						out:fly={{ duration: 100, x: 20 }}
 						on:click={() => {
+							if (changeTimeSelected) {
+								confirmTimeClicked();
+							}
 							paused = !paused;
 						}}
 						on:keypress={() => {
+							if (changeTimeSelected) {
+								confirmTimeClicked();
+							}
 							paused = !paused;
 						}}
 					/>
@@ -377,19 +432,25 @@ function stopTimer() {
 			</div>
 		</div>
 	</div>
+    <!-------------------------------------------------- End first column -->
+
+
+
 
 	<!-- SECOND COLUMN -->
 	<div class="flex flex-col h-full w-[55%] font-sans">
 		<!-- Categories Heading -->
+        <!-- At some point, if it makes sense, have option to add category or increase number of categories -->
 		<div class="w-full border-4 border-l-0 border-neutral-900 h-[6%] flex items-center py-5 justify-between">
 			<h1 class="text-2xl m-2 font-semibold">Categories</h1>
 		</div>
 
 		<!-- List of Categories -->
+        <!-- Categories can run and will be replaced with "Out of categories" -->
 		<ol class="w-full flex flex-col p-2 border-4 border-neutral-900 border-t-0 h-full border-l-0 " transition:slide>
 			{#each categories as category, index}
 				<li class="text-3xl py-2 border-b font-semibold relative w-fit" out:fade>
-					{++index} <span class="text-5xl mx-4">{category ? category : "Out of Categories"}</span>
+					{++index} <span class="text-5xl mx-4">{category ? category : "Out of categories"}</span>
 					{#if paused}
 						<div class="absolute inset-0 bg-[#222222]" in:slide={{ delay: index * 40 }} out:slide />
 					{/if}
@@ -397,8 +458,13 @@ function stopTimer() {
 			{/each}
 		</ol>
 	</div>
+    <!--------------------- End Second Column -->
+
+
+
 
 	<!-- THIRD COLUMN, RESET -->
+    <!-- Pretty straight forward here. -->
 	<div
 		class="h-full border-4 border-neutral-900 border-l-0 w-[15%] flex justify-center items-center cursor-pointer {refreshHovered ? 'dots' : ''}"
 		on:mouseenter={() => {
@@ -409,6 +475,7 @@ function stopTimer() {
 		}}
 		on:click={() => {
 			animateRefresh();
+            // Break in between to allow screen to be covered before changing UI
 			setTimeout(() => {
 				categories = refreshCategories();
 				time = resetTo;
@@ -424,27 +491,43 @@ function stopTimer() {
 			}, 600);
 		}}
 	>
+    <!-- Refresh Spins and Words Drop down. -->
 		<div class="flex flex-col justify-center items-center">
 			<img
 				src="refresh.png"
 				alt="Refresh List"
-				class="w-20 transition-all duration-300 {time === 0 ? "animate-bounce":""} {spinRefresh ? 'rotate-180 opacity-0' : 'rotate-0 opacity-100'}"
+				class="w-20 transition-all duration-300 {time === 0 ? 'animate-bounce' : ''} {spinRefresh ? 'rotate-180 opacity-0' : 'rotate-0 opacity-100'}"
 			/>
 			<h4 class="text-3xl my-5 {spinRefresh ? 'translate-y-4 opacity-0' : 'translate-y-0 opacity-100'} transition-all duration-300">New List</h4>
 		</div>
 	</div>
+    <!-------- End Third Column ------------->
+
+
+
+
+    <!-- TIMES UP and RESETTING overlay. Swipe in and out! -->
 	{#if showReset}
-		<div class="absolute inset-0 bg-[#222222] w-full opactiy-100 flex items-center justify-center" transition:slide={{ duration: 400, axis: "x" }} >
-        <h1 class="text-9xl text-white font-bold">{alertTime ? "Time's Up": "Restarting..."}</h1>
-    </div>
+		<div class="absolute inset-0 bg-[#222222] w-full opactiy-100 flex items-center justify-center" transition:slide={{ duration: 400, axis: "x" }}>
+			<h1 class="text-9xl text-white font-bold">{alertTime ? "Time's Up" : "Restarting..."}</h1>
+		</div>
 	{/if}
+    <!----------End Swipe Overlay----------->
 </div>
+
+
+<!-- Shout out friend :) -->
 <p class="text-xl absolute bottom-4 mx-12">
-	Remake of Swellgarfo's <span class="underline hover:underline-offset-4 "
+	Remake of Swellgarfo's <span class="underline underline-offset-2 hover:underline-offset-4 transition-all"
 		><a href="https://swellgarfo.com/scattergories/">Scattergories List Generator</a></span
 	>
 </p>
 
+
+
+
+
+<!-- Some shit I snatched from the internet :demon: -->
 <style>
 	.stripes {
 		background-image: repeating-linear-gradient(45deg, transparent, #2b2b2b33 12px, transparent 0px, transparent 0px);
